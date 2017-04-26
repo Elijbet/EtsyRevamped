ef248702ada9dbafd787e360a79206d7cdd5bbd8
Eliza Khachatryan <elijbet@gmail.com>
2017-04-26 15:36:04 -0700

    syntax error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
-    :aws_secret_access_key  => ENV["aws_secret_access_key"]
+    :aws_secret_access_key  => ENV["aws_secret_access_key"],
     :region									=> 'us-west-1'
   }
   config.fog_directory  = ENV["etsyrevamped"]                     # required
