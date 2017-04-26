56c14e8f1f2cccbad82e6eaea53f0d6d816976ba
Eliza Khachatryan <elijbet@gmail.com>
2017-04-26 15:34:08 -0700

    region is not a secret key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
-    :aws_secret_access_key  => ENV["aws_secret_access_key"],
-    :aws_region							=> ENV["s3_region"] 
+    :aws_secret_access_key  => ENV["aws_secret_access_key"]
+    :region									=> 'us-west-1'
   }
   config.fog_directory  = ENV["etsyrevamped"]                     # required
 end
