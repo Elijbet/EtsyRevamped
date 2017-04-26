6aa93de45b7ec096cb54fa2af739c01d42dde062
Eliza Khachatryan <elijbet@gmail.com>
2017-04-26 14:07:15 -0700

    added aws regtion

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      =>  ENV["aws_access_key_id"],                        # required
-    :aws_secret_access_key  =>  ENV["aws_secret_access_key"]                        # required
+    :aws_secret_access_key  =>  ENV["aws_secret_access_key"]
+    :aws_region							=>  ENV["aws_region"]                        # required
   }
   config.fog_directory  = ENV["etsyrevamped"]                     # required
 end
