7329fadaf0d8cc9e171c9707b4ccc7cc89d12a7a
Eliza Khachatryan <elijbet@gmail.com>
2017-04-26 15:06:09 -0700

    aws reconfigured

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      =>  ENV["aws_access_key_id"],                        # required
-    :aws_secret_access_key  =>  ENV["aws_secret_access_key"]
-    :aws_region							=>  ENV["aws_region"]                        # required
+    :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
+    :aws_secret_access_key  => ENV["aws_secret_access_key"]
+    :aws_region							=> ENV["aws_region"] 
+    :S3_bucket							=> ENV["etsyrevamped"]                  # required
   }
-  config.fog_directory  = ENV["etsyrevamped"]                     # required
+  # config.fog_directory  = ENV["etsyrevamped"]                     # required
 end
