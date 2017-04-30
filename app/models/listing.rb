da6a2d551467b9c6ea7fa73bbdaf7a32f6079e19
Eliza Khachatryan <elijbet@gmail.com>
2017-04-30 14:24:53 -0700

    Add data validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
 	mount_uploader :image, ImageUploader
 	has_many :reviews
+
+	validates :name, :description, :price, :image, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :reviews, dependent: :destroy     
+
+	validates :first_name, :last_name, presence: true
+
+	  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    message: "can only be a whole number between 1 and 5"
+  }
+  
 end
