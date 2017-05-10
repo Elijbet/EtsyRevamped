de7963ca2ed15c94365ce02c5a2256db20e04be5
Eliza Khachatryan <elijbet@gmail.com>
2017-05-09 18:43:54 -0700

    seller history, sales and purchases pages

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,17 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :check_user, except: [:index, :show, :new, :create]
+  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
   
+
+  def seller
+    @listings = Listing.where(user: current_user).order("create_at DESC")
+  end
 
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
 
+  get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
+  
   root 'listings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
